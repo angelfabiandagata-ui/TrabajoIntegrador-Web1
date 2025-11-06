c619d9970e0504beae5151b876f97ded3dab03e9
angelfabiandagata-ui <angelfabiandagata@gmail.com>
2025-11-06 09:33:52 -0300

    Mejoras en el carrusel

--- js/carrusel.js
+++ js/carrusel.js
@@ -1,28 +1,28 @@
 /*Carrusel*/
 
-let slideIndex = 1;
-showSlides(slideIndex);
+let slideIndex = 1; //Declara e inicializa el carrusel
+carrusel(slideIndex);
 
-function plusSlides(n) {
-  showSlides(slideIndex += n);
+function plusSlides(n) { 
+  carrusel(slideIndex += n);
+}                               
+
+function currentSlide(n) {   
+  carrusel(slideIndex = n);
 }
 
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
-
-function showSlides(n) {
+function carrusel(n) {  //Funcion principal
   let i;
-  let slides = document.getElementsByClassName("mySlides");
-  let dots = document.getElementsByClassName("dot");
+  let slides = document.getElementsByClassName("slides"); //Aca hacemos la conexion con los div de imagenes
+  let dots = document.getElementsByClassName("dot"); //Conexion con los botones de la parte inferior
   if (n > slides.length) {slideIndex = 1}    
   if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
+  for (i = 0; i < slides.length; i++) { //Aca nos aseguramos que las imagenes esten ocultas
     slides[i].style.display = "none";  
   }
   for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
+    dots[i].className = dots[i].className.replace(" active", ""); //Con esto dejamos seleccionado el boton segun la imagen
   }
-  slides[slideIndex-1].style.display = "block";  
-  dots[slideIndex-1].className += " active";
+  slides[slideIndex-1].style.display = "block";  //Muestra la imagen Actual
+  dots[slideIndex-1].className += " active"; //Activa el punto indicador correspondiente
 }
