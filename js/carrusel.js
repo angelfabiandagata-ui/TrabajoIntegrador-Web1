8d6ea6a9d22a67135c58a077f350bba1dac1ee7e
angelfabiandagata-ui <angelfabiandagata@gmail.com>
2025-11-07 19:28:36 -0300

    Mejoras en el Carrusel

--- js/carrusel.js
+++ js/carrusel.js
@@ -1,7 +1,18 @@
 /*Carrusel*/
 
+//Array
+const carouselData = [
+    { title: 'Dulzura', src: './img/carrusel-1.jpeg' }, 
+    { title: 'Técnica', src: './img/carrusel-2.jpg' },
+    { title: 'Profesionalismo', src: './img/carrusel-3.jpg' },
+    { title: 'Dedicación', src: './img/carrusel-4.jpeg' },
+    { title: 'Amor', src: './img/carrusel-5.jpg' }
+];
+
+
 let slideIndex = 1; //Declara e inicializa el carrusel
 carrusel(slideIndex);
+
 
 function plusSlides(n) { 
   carrusel(slideIndex += n);
@@ -12,17 +23,23 @@ function currentSlide(n) {
 }
 
 function carrusel(n) {  //Funcion principal
-  let i;
+  const numSlides = carouselData.length; 
   let slides = document.getElementsByClassName("slides"); //Aca hacemos la conexion con los div de imagenes
-  let dots = document.getElementsByClassName("dot"); //Conexion con los botones de la parte inferior
+  let dots = document.getElementsByClassName("dot"); //Conexion con los botones de la parte inferior3
+  if (slides.length === 0) return; 
+
   if (n > slides.length) {slideIndex = 1}    
   if (n < 1) {slideIndex = slides.length}
+
   for (i = 0; i < slides.length; i++) { //Aca nos aseguramos que las imagenes esten ocultas
     slides[i].style.display = "none";  
   }
   for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", ""); //Con esto dejamos seleccionado el boton segun la imagen
+    dots[i].classList.remove("active");//Con esto dejamos seleccionado el boton segun la imagen
   }
   slides[slideIndex-1].style.display = "block";  //Muestra la imagen Actual
-  dots[slideIndex-1].className += " active"; //Activa el punto indicador correspondiente
+  if (dots.length > 0) {
+        // Usamos classList.add() para evitar problemas de espacios
+        dots[slideIndex-1].classList.add("active"); 
+  }
 }
